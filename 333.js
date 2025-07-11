c380c0177ae0ed11ec103ea8edfff48ff8ee2883
linC333 <2406147586@qq.com>
2025-07-11 19:10:33 +0800

    增加注释//计算1到100的和

--- 333.js
+++ 333.js
@@ -7,7 +7,7 @@ console.log(sum); // 输出结果为5050
 ```
 // 这个 for 循环会计算 1 到 100 的和，并输出结果。
 // 写一个完整的 JavaScript 程序来计算 1 到 100 的和，并输出结果。```javascript name=calculateSum.js
-function calculateSum() {
+function calculateSum() {// 计算1到100的和
     let sum = 0;
     for (let i = 1; i <= 100; i++) {
         sum += i;
